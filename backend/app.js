0037acd3d90c05384d125e72847590f15309c70a
Soobarna <subarnapokharel252@gmail.com>
2024-04-24 15:06:59 +0545

    change

--- backend/app.js
+++ backend/app.js
@@ -23,6 +23,12 @@ app.use(express.urlencoded({extended:true}));
 
 app.use('/api/v1/reservation',reservationRouter)
 
+app.get("/",(req,res,next)=>{
+    return res.status(200).json({
+        success:true,
+        message:"HELLO WORLD",
+    })
+})
 dbConnection();
 
 
