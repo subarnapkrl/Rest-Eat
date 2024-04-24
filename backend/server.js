2d285e40e1e0ecf019e1c0f8846a3d6b57cdbcea
Soobarna <subarnapokharel252@gmail.com>
2024-04-24 15:51:56 +0545

    commit

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,6 @@
 import app from "./app.js";
 
-const express = require("express");
-const cors = require("cors");
-const app = express();
 
-app.use(cors());
-app.get("/", (req,res)=>{
-res.send("server working");
-});
 
 app.listen(process.env.PORT,()=>{
     console.log(`Server running on Port : ${process.env.PORT}`)
