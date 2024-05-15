61470342c62df7f8554ec8545218312a525a93fa
Soobarna <subarnapokharel252@gmail.com>
2024-05-15 11:36:01 +0545

    cors

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ dotenv.config({path:"./config/config.env"});
 
 
 app.use(cors({
-    origin:[process.env.FRONTEND_URL],
+    origin:'https://rest-eat.vercel.app',
     methods:["POST"],
     credentials:true
 }))
